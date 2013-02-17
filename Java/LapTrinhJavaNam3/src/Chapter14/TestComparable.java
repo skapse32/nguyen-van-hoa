5d4785a90de51c5c9c9e01ee21e206da8c67ed21
nvh0412 <nvh0412@gmail.com>
2013-02-17 02:20:44 +0000

    Beginning chap14

---
+++ Java/LapTrinhJavaNam3/src/Chapter14/TestComparable.java
@@ -0,0 +1,15 @@
+package Chapter14;
+
+public class TestComparable {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ComparableRectangle o1 = new ComparableRectangle(4,5);
+		ComparableRectangle o2 = new ComparableRectangle(3,6);
+		System.out.println(o1.compareTo(o2));
+	}
+
+}
