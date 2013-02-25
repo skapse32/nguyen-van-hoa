d31a1d28c54b705db68fc0020b2a9e67f8b7fdf3
nvh0412 <nvh0412@gmail.com>
2013-02-25 17:22:41 +0000

    Completed knowledge of this chapter!!!.Congrulation for me.Au ze!.

--- Java/LapTrinhJavaNam3/src/Chapter21/TestGenericMatrix.java
+++ Java/LapTrinhJavaNam3/src/Chapter21/TestGenericMatrix.java
@@ -2,6 +2,10 @@ package Chapter21;
 
 public class TestGenericMatrix {
 	public static void main(String[] args){
-		IntegerMatrix m1 = new IntegerMatrix();
+		Integer[][] m1 = {{1,2,3},{4,5,6},{1,1,1}};
+		Integer[][] m2 = {{1,1,1},{2,2,2},{0,0,0}};
+		IntegerMatrix matrix = new IntegerMatrix();
+		
+		GenericMatrix.printResult(m1, m2, matrix.addMatrix(m1, m2), '+');
 	}
 }
