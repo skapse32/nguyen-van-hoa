041d6650ffb1e1fa74f7b191540fec8b17bf8879
nvh0412@gmail.com <nvh0412@gmail.com@6252d10e-679a-22fa-8d87-ab3efc276808>
2013-02-27 00:06:52 +0000

    Beginning OOP Tuan3

--- trunk/Java/LapTrinhJavaNam3/src/Chapter16/ControlCircle.java
+++ trunk/Java/LapTrinhJavaNam3/src/Chapter16/ControlCircle.java
@@ -1,7 +1,17 @@
 package Chapter16;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.RenderingHints.Key;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,7 +23,21 @@ public class ControlCircle extends JFrame {
 	private JButton btn_phongto;
 	private JButton btn_thunho;
 	private CirclePanel myCircle = new CirclePanel();
+	private ButtonListener myListener = new ButtonListener();
 
+	class ButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			if (e.getSource() == btn_phongto)
+				myCircle.phongto();
+			else if (e.getSource() == btn_thunho) {
+				myCircle.thunho();
+			}
+		}
+
+	}
 
 	public ControlCircle() {
 		// TODO Auto-generated constructor stub
@@ -23,10 +47,34 @@ public class ControlCircle extends JFrame {
 		btn_thunho = new JButton("Thu Nho");
 		p1.add(btn_phongto);
 		p1.add(btn_thunho);
-		
-		
+
 		add(myCircle, BorderLayout.CENTER);
-		add(p1 , BorderLayout.SOUTH);
+		add(p1, BorderLayout.SOUTH);
+
+		btn_phongto.addActionListener(myListener);
+		btn_thunho.addActionListener(myListener);
+
+		addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowActivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+				super.windowActivated(e);
+				System.out.println("Window Actived");
+			}
+
+		});
+
+		/*
+		 * addMouseMotionListener(new MouseMotionAdapter() {
+		 * 
+		 * @Override public void mouseDragged(MouseEvent e) { // TODO
+		 * Auto-generated method stub super.mouseDragged(e);
+		 * myCircle.setX(e.getX()); myCircle.setY(e.getY());
+		 * myCircle.dragCircle(); }
+		 * 
+		 * });
+		 */
 	}
 
 	/**
@@ -42,22 +90,71 @@ public class ControlCircle extends JFrame {
 		myFrame.setLocationRelativeTo(null);
 		myFrame.setVisible(true);
 	}
-	
+
 	class CirclePanel extends JPanel {
-		private int radius = 20;
+		private int radius = 5;
+		private int x;
+		private int y;
+
+		public CirclePanel() {
+			super();
+			// TODO Auto-generated constructor stub
+			x = getWidth() / 2 - radius;
+			y = getHeight() / 2 - radius;
+
+			addKeyListener(new KeyAdapter() {
+
+				@Override
+				public void keyPressed(KeyEvent e) {
+					// TODO Auto-generated method stub
+					super.keyPressed(e);
+					if (e.getKeyCode() == KeyEvent.VK_UP) {
+						x++;
+					}
+				}
+			});
+			repaint();
+		}
+
+		public int getX() {
+			return x;
+		}
+
+		public void setX(int x) {
+			this.x = x;
+		}
+
+		public int getY() {
+			return y;
+		}
+
+		public void setY(int y) {
+			this.y = y;
+		}
 
 		public void phongto() {
 			radius++;
 			repaint();
 		}
 
-		@Override
-		public void paintComponents(Graphics g) {
-			// TODO Auto-generated method stub
-			super.paintComponents(g);
-			g.drawOval(getWidth() / 2 - radius, getHeight() / 2 - radius,
-					radius * 2, radius * 2);
+		public void thunho() {
+			radius--;
+			repaint();
 		}
 
+		public void dragCircle() {
+			x++;
+			repaint();
+		}
+
+		@Override
+		public void paint(Graphics arg0) {
+			// TODO Auto-generated method stub
+			super.paint(arg0);
+
+			arg0.drawOval(x, y,
+					radius * 2, radius * 2);
+		}
+		
 	}
 }
