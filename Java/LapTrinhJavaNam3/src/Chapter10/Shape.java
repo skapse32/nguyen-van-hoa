a2af71d00e47a6d3c279f68099396548708addf6
nvh0412 <nvh0412@gmail.com>
2013-02-27 02:01:55 +0000

    Completed inheritance

---
+++ Java/LapTrinhJavaNam3/src/Chapter10/Shape.java
@@ -0,0 +1,23 @@
+package Chapter10;
+
+public class Shape {
+
+	private String color;
+	
+	public Shape(String color) {
+		// TODO Auto-generated constructor stub
+		this.color = color;
+	}
+
+	public double getArea(){
+		return 0;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Color : " + color;
+	}
+	
+	
+}
