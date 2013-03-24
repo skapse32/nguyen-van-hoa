e99be4b67ab75c926ee1ed1c846f308b5c63cec4
nvh0412 <nvh0412@gmail.com>
2013-03-24 07:59:54 +0000

    Bai Tap GUI Shap and Color

---
+++ Java/BTGUI2/src/com/nvh/GUI/FreeDrawing.java
@@ -0,0 +1,89 @@
+package com.nvh.GUI;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.border.TitledBorder;
+
+public class FreeDrawing extends JFrame{
+
+	/**
+	 * @param args
+	 */
+	private JRadioButton rdb_red;
+	private JRadioButton rdb_green;
+	private JRadioButton rdb_blue;
+
+	private JRadioButton rdb_line;
+	private JRadioButton rdb_circle;
+	private JRadioButton rdb_rectangle;
+	private JButton save;
+	private JButton open;
+	private JButton clear;
+	private JButton exit;
+	
+	public FreeDrawing() {
+		super();
+		//Panel chinh
+		JPanel contextPanel = new JPanel(new BorderLayout());
+		
+		//Panel cac tool tuy chinh
+		JPanel toolPanel = new JPanel(new GridLayout(2, 1));
+		
+		//Panel color
+		JPanel color = new JPanel(new GridLayout(3, 1));
+		color.add(rdb_red = new JRadioButton("Red"));
+		color.add(rdb_green = new JRadioButton("Green"));
+		color.add(rdb_blue = new JRadioButton("Blue"));
+		color.setBorder(new TitledBorder("Color"));
+		ButtonGroup btg_color = new ButtonGroup();
+		btg_color.add(rdb_red);
+		btg_color.add(rdb_green);
+		btg_color.add(rdb_blue);
+		toolPanel.add(color);
+		
+		//Panel Shape
+		JPanel shape = new JPanel(new GridLayout(3,1));
+		shape.add(rdb_line = new JRadioButton("Line"));
+		shape.add(rdb_circle = new JRadioButton("Circle"));
+		shape.add(rdb_rectangle = new JRadioButton("Rectangle"));
+		shape.setBorder(new TitledBorder("Shape"));
+		ButtonGroup btg_shape = new ButtonGroup();
+		btg_shape.add(rdb_line);
+		btg_shape.add(rdb_circle);
+		btg_shape.add(rdb_rectangle);
+		toolPanel.add(shape);
+		
+		//panel khung ve
+		JPanel mContent = new JPanel();
+		mContent.setBorder(new TitledBorder(""));
+		mContent.setBackground(Color.WHITE);
+		
+		//panel button
+		JPanel mbutton = new JPanel(new GridLayout(1, 4, 5 ,5));
+		mbutton.add(save = new JButton("Save"));
+		mbutton.add(open = new JButton("Open"));
+		mbutton.add(clear = new JButton("Clear"));
+		mbutton.add(exit = new JButton("Exit"));
+		
+		contextPanel.add(mContent, BorderLayout.CENTER);
+		contextPanel.add(toolPanel,BorderLayout.EAST);
+		contextPanel.add(mbutton,BorderLayout.SOUTH);
+		
+		add(contextPanel);
+		pack();
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new FreeDrawing();
+	}
+
+}
