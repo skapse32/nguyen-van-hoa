06a9135c1c54e6225b76bda09a3cdc5a583cbf4d
nvh0412@gmail.com <nvh0412@gmail.com@6252d10e-679a-22fa-8d87-ab3efc276808>
2013-05-24 01:23:38 +0000

    State Pattern

---
+++ trunk/Java/HeadFirstDP/src/StatePattern/SoldOutState.java
@@ -0,0 +1,29 @@
+package StatePattern;
+
+public class SoldOutState implements State {
+
+	@Override
+	public void insertQuater() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ejectQuater() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnCrank() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispense() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
