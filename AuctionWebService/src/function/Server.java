8f79635ea9ffd1e35819ae73a942871df4809e8e
nvh0412 <nvh0412@gmail.com>
2013-10-08 10:18:47 +0000

    upload

---
+++ AuctionWebService/src/function/Server.java
@@ -0,0 +1,8 @@
+package function;
+
+public class Server {
+
+	public static String addressAuthenWS="http://192.168.1.4:8080/auth/services/";
+	public static String saveDirectory="E:/HK8/LVTN/workspaceKL/.metadata/.me_tcat/webapps/AuctionWebService/resources/images/product/";
+
+}
